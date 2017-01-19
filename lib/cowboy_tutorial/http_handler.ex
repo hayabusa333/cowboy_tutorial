6d0512d82ddd8cda370e18c69da14a0755050cee
hayabusa333 <junnichi0121@gmail.com>
2017-01-19 14:08:57 +0900

    add a contact page

--- lib/cowboy_tutorial/http_handler.ex
+++ lib/cowboy_tutorial/http_handler.ex
@@ -6,17 +6,21 @@ defmodule CowboyTutorial.HttpHandler do
     {:ok, resp, opts}
   end
 
-  def html_respons("GET", param, req) when (param == "home" or param == "help") do
-    headers = %{"content-type" => "text/html"}
-    {:ok, file} = File.read "priv/static/html/#{param}.html"
-    body = file
-    {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
+  def html_read(param) do
+    case File.read "priv/static/html/#{param}.html" do
+      {:ok, file} ->
+        body = file
+        {200, body}
+      {:error, _} ->
+        body = ""
+        {404, body}
+    end
   end
 
   def html_respons("GET", param, req) do
     headers = %{"content-type" => "text/html"}
-    body = ""
-    {:ok, resp} = :cowboy_req.reply(404, headers, body, req)
+    {states, body} = html_read(param)
+    {:ok, resp} = :cowboy_req.reply(states, headers, body, req)
   end
 
 end
